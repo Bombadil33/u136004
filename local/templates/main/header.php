7753b7f7b042d63d81f0696ee54cdd0333daeb26
Victor Andrianov <viktorad@yandex.ru>
2015-09-20 13:05:17 +0300

    Logo and Login

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -41,11 +41,7 @@
         </h1>
         <!-- Cart -->
         <div id="cart">
-            <a href="#" class="cart-link">Your Shopping Cart</a>
-            <div class="cl">&nbsp;</div>
-            <span>Articles: <strong>4</strong></span>
-            &nbsp;&nbsp;
-            <span>Cost: <strong>$250.99</strong></span>
+            <a href="#" class="cart-link">Вход/Регистрация</a>
         </div>
         <!-- End Cart -->
 
